88622e51bd4c2b386d7d4b3e124c580d8fed6a35
deadfiesta <61130146+deadfiesta@users.noreply.github.com>
2022-08-19 02:44:15 +0800

    Sass attempt

--- pages/motion.js
+++ pages/motion.js
@@ -1,6 +1,11 @@
 const motion = () => {
   return (
-    <h2>Motion testing</h2>
+    <>
+      <header>
+        <h2>Motion Guidelines</h2>
+        <p>Motion in UI guides and respond to the user. Motion design enhances and faciliates the connection between actions and expected reactions on the user side. It drives user’s attention to particular object and hints at what will happen with each action performed by the user.</p>
+      </header>
+    </>
   )
 }
 
