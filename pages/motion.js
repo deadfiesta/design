01fe563b572f5f35f7a8edc1c3094c1961a98585
deadfiesta <61130146+deadfiesta@users.noreply.github.com>
2022-08-19 02:56:05 +0800

    with scss

--- pages/motion.js
+++ pages/motion.js
@@ -1,7 +1,9 @@
+import main from '../styles/Main.module.scss'
+
 const motion = () => {
   return (
     <>
-      <header>
+      <header className={main.header}>
         <h2>Motion Guidelines</h2>
         <p>Motion in UI guides and respond to the user. Motion design enhances and faciliates the connection between actions and expected reactions on the user side. It drives user’s attention to particular object and hints at what will happen with each action performed by the user.</p>
       </header>
