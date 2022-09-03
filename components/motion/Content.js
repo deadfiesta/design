8e05d43609cc07c152d723c8165dbb5b48d77f55
deadfiesta <61130146+deadfiesta@users.noreply.github.com>
2022-09-03 13:24:56 +0800

    Updated10

--- components/motion/Content.js
+++ components/motion/Content.js
@@ -7,7 +7,7 @@ import Section from '../motion/Section'
 import Principle from '../motion/Principle'
 import SpinnerCircle from './components/SpinnerCircle'
 import SwitchLightDark from './components/SwitchLightDark'
-import MenuOpen from './components/MenuOpen'
+import MenuOpen from './components/MenuOpenSampleCrazy'
 //Why Spring 
 import WhySpring from './WhySpring'
 import SpringPropsDemo from './components/SpringPropsDemo'
